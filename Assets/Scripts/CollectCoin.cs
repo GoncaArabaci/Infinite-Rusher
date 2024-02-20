c87dc5b64a00f63431a60da7d9e280214182819d
Gonca <goncaarabaci35@gmail.com>
2024-02-20 16:38:48 +0300

    Added Score Panel

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectCoin : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int score;
+    public TextMeshProUGUI scoreText;
+    int increaseRate = 1;
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    void FixedUpdate()
     {
-        
+        UpdateScore();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
+            AddCoin();
             Destroy(other.gameObject);
         }
+    }
+    public void AddCoin()
+    {
+        score += 50;
+        scoreText.text = score.ToString();
+    }
+    public void UpdateScore()
+    {
+        score += increaseRate;
+        scoreText.text = score.ToString();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,30 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public float runningSpeed;
     float xPos = 0;
-    public float xSpeed;
     public float xLimit;
     float rightPos = 2f;
     float leftPos = -2f;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(leftPos, 0f, 0f);
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(rightPos, 0f, 0f);
         }
 
-        xPos = transform.position.x * xSpeed * Time.deltaTime;
         xPos = Mathf.Clamp(xPos, -xLimit, xLimit);
 
         Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + runningSpeed *Time.deltaTime);
