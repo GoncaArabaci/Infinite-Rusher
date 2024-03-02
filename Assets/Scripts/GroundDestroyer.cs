89cde01d692a719bc939a1063270f5882e22036d
Gonca <goncaarabaci35@gmail.com>
2024-03-02 23:18:44 +0300

    Added Destroyer

---
+++ Assets/Scripts/GroundDestroyer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDestroyer : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
